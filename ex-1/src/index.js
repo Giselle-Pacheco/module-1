5da4cd9eefa1ec835c8a25b87d44b6a377fd8930
GiselleP <pachecog2706@gmail.com>
2023-09-18 19:48:41 -0600

    exercise 1 done

--- ex-1/src/index.js
+++ ex-1/src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Header=(props)=>{
+  return(
+    <div>
+      <p>
+        <h1>{props.header}</h1>
+      </p>  
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Content=(props)=>{
+  return(
+    <div>
+      <p>
+        Content: 
+          <p>Part1 = {props.part1}, number of exercises {props.exercises1}</p>
+          <p>Part2 = {props.part2}, number of exercises {props.exercises2}</p>
+          <p>Part3 = {props.part3}, number of exercises {props.exercises3}</p>
+
+      </p>
+    </div>
+  )
+}
+
+const Total=(props)=>{
+  return(
+    <div>
+      <p>
+        Total: {props.total}
+      </p>
+    </div>
+
+  )
+}
+const App = () => {
+  const header='Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total=exercises1+exercises2+exercises3
+
+  return (
+    <div>
+      <Header header={header}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2}  part3={part3} exercises3={exercises3} />
+      <Total total={total} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
