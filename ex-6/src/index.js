aa0557a7b027644cd58d907bd4b2191e50d65c4b
GiselleP <pachecog2706@gmail.com>
2023-09-19 22:53:10 -0600

    exercise 1.9 done

--- ex-6/src/index.js
+++ ex-6/src/index.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const History = (props) => {
+  if (props.comments.length === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Statistics good={props.good} neutral={props.neutral} bad={props.bad} total={props.total}/>
+    </div>
+  )
+}
 
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
@@ -10,16 +25,13 @@ const Button = ({ onClick, text }) => (
 
 const Statistics=(props)=>{
   return(
-    <div>
-      <p>
-        <h1>statistics</h1>
-        <p>good: {props.good}</p>
-        <p>neutral: {props.neutral}</p>
-        <p>bad: {props.bad}</p>
-        <p>all {props.total}</p>
-        <p>average {(props.total)/3}</p>
-        <p>positive {props.good/props.total}</p> 
-      </p>  
+    <div>      
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all {props.total}</p>
+      <p>average {(props.total)/3}</p>
+      <p>positive {props.good/props.total}</p> 
     </div>
   )
 }
@@ -29,28 +41,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [comments, AllComments]=useState([])
   
 
   const handleGoodClick = () => {
+    AllComments(comments+1)
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
+    AllComments(comments+1)
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
+    AllComments(comments+1)
     setBad(bad + 1)
   }
 
   const total=good+bad+neutral
-
-  // {props.good_status}
- // <p>neutral {neutral}</p>
-// <p>bad {bad}</p>
-// <p>all {total}</p>
-// <p>average {(total)/3}</p>
-// <p>positive {good/total}</p>
 
 
   return (
@@ -61,8 +70,9 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
 
-      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+      <h1>statistics</h1>
       
+      <History  comments={comments} good={good} neutral={neutral} bad={bad} total={total}/>
 
       
       
